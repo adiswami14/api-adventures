3675fd2bb8ae5c4d61d0ae6f95585df6deab8468
adiswami14 <adithya1431@gmail.com>
2020-09-19 18:39:39 -0400

    Make separate runner classes for CLI and API

---
+++ src/main/java/CLIRunner.java
@@ -0,0 +1,14 @@
+import student.adventure.CommandLineAdventure;
+import student.adventure.JSONReader;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CLIRunner {
+    public static void main(String[] args) throws IOException {
+        JSONReader reader = new JSONReader(new File("src/main/resources/data/Data.json"));
+        CommandLineAdventure adventure = new CommandLineAdventure(reader.getGame());
+        adventure.run();
+        adventure.quit();
+    }
+}
