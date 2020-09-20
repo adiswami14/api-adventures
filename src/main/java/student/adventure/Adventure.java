2bca4378229cd78c51e91dd6531682d2cc1bc797
adiswami14 <adithya1431@gmail.com>
2020-09-19 23:10:00 -0400

    Create a separate Adventure file for API

--- src/main/java/student/adventure/Adventure.java
+++ src/main/java/student/adventure/Adventure.java
@@ -1,20 +1,39 @@
 package student.adventure;
 
+import student.server.Command;
+import student.server.GameStatus;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Main adventure class that is used to interact with the API
+ */
 public class Adventure {
-    private Scanner scanner; //scanner for the game
     private Player player;  //Player object of this game
     private ArrayList<Room> rooms;  //list of rooms in the game
     private Room currentRoom; //current Room that Player is in, used for brevity
+    private GameStatus status; //current status of the Adventure game
+    private Command command; //command currently issued by the client
+    private int id; //id for this Adventure instance
 
     /**
-     * @param game instance of Game wrapper class, used to extract list of rooms
+     * Constructor for the API adventure class
      */
-    public Adventure(Game game) {
-        scanner = new Scanner(System.in);
-        this.rooms = game.getRooms();
+    public Adventure(int id) {
+        JSONReader reader = null;
+        try { //made a try-catch block to not interfere with AdventureService method's signature
+            reader = new JSONReader(new File("src/main/resources/data/Data.json"));
+            this.rooms = reader.getGame().getRooms();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.id = id;
+        status = new GameStatus(false, id, "Hey there!", null, null, null,
+                null);
     }
 
     /**
@@ -25,25 +44,51 @@ public class Adventure {
     }
 
     /**
+     * Gets current status of the game
+     * @return GameStatus object of this game
+     */
+    public GameStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * @return id of the game
+     */
+    public int getId(){
+        return id;
+    }
+
+    /**
+     * Sets command given by client to current command issued to the game
+     * @param command the command passed in by the client
+     */
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    /**
      * Method that is called to start the game
      */
     public void run(){
         initializePlayer();
-        examine(currentRoom);   //get description of the room you start of in
-        while(!isGameOver()) {
-            handleUserInput(scanner.nextLine());
-        }
-        System.out.println(currentRoom.getDescription());  //get description of the last room
+        examine();   //get description of the room you start of in
+        handleUserInput();
+        //ioHandler.println(currentRoom.getDescription());  //get description of the last room
+    }
+
+    /**
+     * Method for quitting the program
+     * Seems unnecessary, but it improves readability
+     */
+    public void quit() {
+
     }
 
     /**
      * Initializes basic player fields such as name and currentRoom
      */
     private void initializePlayer(){
-        System.out.println("Hey player, what's your name: ");
-
-        player = new Player(scanner.nextLine().trim());
-        System.out.print("Hey, "+player.getName()+"! ");
+        player = new Player("");
         player.setCurrentRoom(rooms.get(0));
         currentRoom = player.getCurrentRoom();
         player.getRoomHistory().add(currentRoom.getName()); //adds starting room to room history
@@ -51,14 +96,13 @@ public class Adventure {
 
     /**
      * Aligns with examine() method in assignment description
-     * @param room room we are examining
      */
-    private void examine(Room room) {
-        if(room == null) {
+    private void examine() {
+        if(currentRoom == null) {
             throw new IllegalArgumentException();
         }
-        examineDirections(room); //split examine function into examineDirections and examineItems
-        examineItems(room);
+        examineDirections(currentRoom); //split examine function into examineDirections and examineItems
+        examineItems(currentRoom);
     }
 
     /**
@@ -66,18 +110,18 @@ public class Adventure {
      * @param room room we are examining
      */
     private void examineDirections(Room room) {
-        System.out.println(room.getDescription());  //String we will return for this method
-        System.out.print("From here, you can go: ");
+        //ioHandler.println(room.getDescription());  //String we will return for this method
+        //ioHandler.print("From here, you can go: ");
 
         ArrayList<Direction> directionsInRoom = room.getPossibleDirections();
 
         for(int directionIndex = 0; directionIndex<directionsInRoom.size();directionIndex++) {
-            System.out.print(directionsInRoom.get(directionIndex).getName());
+           // ioHandler.print(directionsInRoom.get(directionIndex).getName());
             if(directionIndex != directionsInRoom.size()-1) {
-                System.out.print(", ");
+               // ioHandler.print(", ");
             }
         }
-        System.out.println();
+        //ioHandler.println();
     }
 
     /**
@@ -85,54 +129,36 @@ public class Adventure {
      * @param room room we are examining
      */
     private void examineItems(Room room) {
-        System.out.print("Items: ");
+       // ioHandler.print("Items: ");
         ArrayList<Item> itemsInRoom = room.getItems();
 
         for(int itemIndex = 0; itemIndex<itemsInRoom.size();itemIndex++) {
-            System.out.print(itemsInRoom.get(itemIndex).getName());
+            //ioHandler.print(itemsInRoom.get(itemIndex).getName());
             if(itemIndex != itemsInRoom.size()-1) {
-                System.out.print(", ");
+                //ioHandler.print(", ");
             }
         }
-        System.out.println();
+        //ioHandler.println();
     }
 
     /**
      * Runs certain functions according to the string passed in
-     * @param userInput the input string the user passes in at certain points of the game
      */
-    private void handleUserInput(String userInput) {
-        String[] strInput = Utils.formatString(userInput).split(" ");
-        String command = strInput[0];
-        String subject = Utils.formatString(Utils.joinStringArray(strInput, 1, strInput.length));
-        //since command is the first element, we only need to join the rest of the string array
+    private void handleUserInput() {
+        Method method;
 
-        switch(command.toLowerCase()) { //checks first word of userInput
-            case "examine":
-                examine(currentRoom);
-                break;
-            case "take":
-                takeItem(subject);
-                break;
-            case "drop":
-                dropItem(subject);
-                break;
-            case "go":
-                goInDirection(subject);
-                break;
-            case "history":
-                history();
-                break;
-            case "distanceto":
-                distanceTo(subject);
-                break;
-            case "quit":  //no break here because quit and exit have exactly the same purpose
-            case "exit":
-                terminate();
-                break;
-            default:
-                System.out.println("I don't understand \""+userInput+"\"!");
-                break;
+        try {   //checks for methods w/ String params
+            method = this.getClass().getDeclaredMethod(command.getCommandName(), String.class);
+            method.setAccessible(true);
+            method.invoke(this, command.getCommandValue());
+        } catch(Exception e) {
+            try { //this is for methods that don't take in any params (i.e. examine(), quit())
+                method = this.getClass().getDeclaredMethod(command.getCommandName(), null);
+                method.setAccessible(true);
+                method.invoke(this);
+            } catch (Exception exception) {
+                //ioHandler.println("I don't understand \""+commandName+"\"!");
+            }
         }
     }
 
@@ -140,7 +166,7 @@ public class Adventure {
      * Player attempts to go in a certain direction
      * @param directionName the name of the direction the user enters
      */
-    private void goInDirection(String directionName) {
+    private void go(String directionName) {
         for(Direction direction : currentRoom.getPossibleDirections()) {
 
             if(direction.getName().toLowerCase().equals(directionName.toLowerCase())) {
@@ -156,14 +182,14 @@ public class Adventure {
                 }
             }
         }
-        System.out.println("I can't go \""+directionName+"\"!");
+        //ioHandler.println("I can't go \""+directionName+"\"!");
     }
 
     /**
      * Player attempts to drop a certain item
      * @param itemName name of the item the user enters
      */
-    private void dropItem(String itemName) {
+    private void drop(String itemName) {
         for(Item item : player.getInventory()) {
             if(item.getName().toLowerCase().equals(itemName.toLowerCase())) {
                 currentRoom.getItems().add(item);
@@ -171,14 +197,14 @@ public class Adventure {
                 return;
             }
         }
-        System.out.println("You don't have \""+itemName+"\" in your inventory!");
+        //ioHandler.println("You don't have \""+itemName+"\" in your inventory!");
     }
 
     /**
      * Player attempts to take a certain item
      * @param itemName name of the item the user enters
      */
-    private void takeItem(String itemName) {
+    private void take(String itemName) {
         for(Item item : currentRoom.getItems()) {
             if(item.getName().toLowerCase().equals(itemName.toLowerCase())) {
                 player.getInventory().add(item);
@@ -186,23 +212,23 @@ public class Adventure {
                 return;
             }
         }
-        System.out.println("There is no item \""+itemName+"\" in this room!");
+        //ioHandler.println("There is no item \""+itemName+"\" in this room!");
     }
 
     /**
      * Function that formats and prints out room history of player
      */
     private void history() {
-        System.out.print("Your room history: ");
+        //ioHandler.print("Your room history: ");
         ArrayList<String> playerRoomHistory = player.getRoomHistory();
 
         for(int roomIndex = 0; roomIndex<playerRoomHistory.size();roomIndex++) {
-            System.out.print(playerRoomHistory.get(roomIndex));
+            //ioHandler.print(playerRoomHistory.get(roomIndex));
             if(roomIndex != playerRoomHistory.size()-1) {
-                System.out.print(", ");
+                //ioHandler.print(", ");
             }
         }
-        System.out.println();
+        //ioHandler.println();
     }
 
     /**
@@ -213,13 +239,13 @@ public class Adventure {
 
         for(Room room : rooms) {
             if(room.getName().toLowerCase().equals(roomName.toLowerCase())) {
-                System.out.println("The distance between the current room and the "+roomName+" is "
-                        +Utils.findDistance(currentLoc, room.getLocation())+" meters");
+                //ioHandler.println("The distance between the current room and the "+roomName+" is "
+                        //+Utils.findDistance(currentLoc, room.getLocation())+" meters");
                 return;
             }
         }
 
-        System.out.println("The room name \""+roomName+"\" is invalid");
+        //ioHandler.println("The room name \""+roomName+"\" is invalid");
     }
 
     /**
@@ -231,10 +257,9 @@ public class Adventure {
     }
 
     /**
-     * Method for quitting the program
-     * Seems unnecessary, but it improves readability
+     * Updates gameState of current game
      */
-    public void terminate() {
-        System.exit(0);
+    private void updateGameState() {
+
     }
 }
